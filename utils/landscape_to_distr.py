24169c117b2b85d83d74093b2f7365a33959ea04
Nina-Konovalova <nina.konovalova@phystech.edu>
2021-12-02 10:55:18 +0300

    change functions for m gaussians

--- utils/landscape_to_distr.py
+++ utils/landscape_to_distr.py
@@ -1,19 +1,12 @@
 import subprocess
-from gauss_fit import *
-from data_frotran_utils import *
+from utils.gauss_fit import *
+from utils.data_frotran_utils import *
 
 
 def probabilities_from_init_distributions(x_end):
         best_vals = x_end
-        y = gaussian(np.arange(51), best_vals[0], best_vals[1], best_vals[2],
-                          best_vals[3], best_vals[4], best_vals[5],
-                          best_vals[6], best_vals[7], best_vals[8],
-                          best_vals[9], best_vals[10], best_vals[11])
+        y = gaussian(np.arange(51), *best_vals)
         make_input_file(y)
-        # plt.plot(y, 'go--', linewidth=4, markersize=2,
-        #          color='red', label='parametrized X')
-
-        # fortran programm - making new distributions
         subprocess.check_output(["./outputic"])
         # saving new output data
         rate, y_pos_new, y_neg_new = read_data()
